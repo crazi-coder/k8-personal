4e8498c41f46cf23b653fe55044aff78b61b746b
Abhilash Joseph C <abhilash@infilect.com>
2023-01-13 08:58:26 +0530

    keda addon

--- sample-app/flaskapi.py
+++ sample-app/flaskapi.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import socket   
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    """Function to test the functionality of the API"""
-    return "Hello, world!"
+    hostname=socket.gethostname()   
+    IPAddr=socket.gethostbyname(hostname) 
+    return jsonify({"hostname":hostname, "ipaddr":IPAddr})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
